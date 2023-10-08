437e5e85bcbde0b755c66da1071ceff44217d781
goga2420 <58983503+goga2420@users.noreply.github.com>
2023-10-08 19:47:47 +0300

    Initial Commit

---
+++ TP3sem1lab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TP3sem1lab
+//
+//  Created by GeorgeZh on 10/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
